bc17ce5b8b28f4cc97927c92c6e405a9cc9523b3
zen811 <khatriharshit84@gmail.com>
2025-10-23 19:24:54 +0530

    initial commit

--- hospital_manage.c
+++ hospital_manage.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
-#define Size 1
+#include <string.h>
+#define max 1
 void show_patient(char (*ptr)[7][20], int Sno);
-void show_list(char (*ptr)[7][20]);
-void add_patient(char (*ptr)[7][20], char pt_info[7]);
+void show_list(char (*ptr)[7][20], int size);
+void add_patient(char (*ptr)[7][20], char pt_info[7], int size);
 void remove_patient();
 int main()
 {
     int i, num;
-    char list[Size][7][20] = {{"S_No", "Name", "Age", "B_Group", "height", "weight", "bmi"}};
+    int size = 1;
+    char list[max][7][20] = {{"S_No", "Name", "Age", "B_Group", "height", "weight", "bmi"}};
     char (*ptr)[7][20] = list;
 
 main_menu:
@@ -16,14 +18,14 @@ main_menu:
     switch (i)
     {
     case 1:
-        show_list(ptr);
+        show_list(ptr, size);
         goto main_menu;
     case 2:
         scanf("%d", &num);
         show_patient(ptr, num);
         goto main_menu;
     case 3:
-        Size++;
+        size++;
         char S_No[20], Name[20], Age[20], B_Group[20];
         float height, weight, bmi;
         printf("Enter S_no");
@@ -40,17 +42,17 @@ main_menu:
         scanf("%f", &weight);
         bmi = weight * 10000 / height * height;
         char pt_inf[7] = {S_No, Name, Age, B_Group, height, weight, bmi};
-        add_patient(list, pt_inf);
+        add_patient(list, pt_inf, size);
 
     default:
         goto main_menu;
     }
 }
 }
-void show_list(char (*ptr)[7][20])
+void show_list(char (*ptr)[7][20], int size)
 {
     int i, j;
-    for (i = 0; i < Size; i++)
+    for (i = 0; i < size; i++)
     {
         for (j = 0; j < 7; j++)
         {
@@ -67,6 +69,11 @@ void show_patient(char (*ptr)[7][20], int Sno)
         printf("%s", ptr[Sno][i]);
     }
 }
-void add_patient(char (*ptr)[7][20], char pt_info[7])
+void add_patient(char (*ptr)[7][20], char pt_info[7], int size)
 {
+    int i, j;
+    for (j = 0; j < 7; j++)
+    {
+        strcpy(ptr[size][j], pt_info[j]);
+    }
 }
