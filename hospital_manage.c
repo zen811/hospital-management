52e9c09dc640e65e5a9c56089785f5b7e7c2ca3c
zen811 <khatriharshit84@gmail.com>
2025-10-23 19:56:08 +0530

    3 functions made

--- hospital_manage.c
+++ hospital_manage.c
@@ -3,7 +3,7 @@
 #define max 1
 void show_patient(char (*ptr)[7][20], int Sno);
 void show_list(char (*ptr)[7][20], int size);
-void add_patient(char (*ptr)[7][20], char pt_info[7], int size);
+void add_patient(char (*ptr)[7][20], char pt_info[7][20], int size);
 void remove_patient();
 int main()
 {
@@ -11,9 +11,11 @@ int main()
     int size = 1;
     char list[max][7][20] = {{"S_No", "Name", "Age", "B_Group", "height", "weight", "bmi"}};
     char (*ptr)[7][20] = list;
+    printf("Welcome to hospital management system By Zen\n");
 
 main_menu:
 {
+    printf("You are on The main menu Please select an option\n1)Show list\n2)show a specific patient\n3)add a patient\n4)remove a patient\n5)exit\n");
     scanf("%d", &i);
     switch (i)
     {
@@ -21,30 +23,43 @@ main_menu:
         show_list(ptr, size);
         goto main_menu;
     case 2:
+        printf("Enter S_no of the patient");
         scanf("%d", &num);
         show_patient(ptr, num);
         goto main_menu;
     case 3:
+        printf("Enter detals of the patient.\n");
         size++;
         char S_No[20], Name[20], Age[20], B_Group[20];
         float height, weight, bmi;
-        printf("Enter S_no");
-        scanf("%s", &S_No);
-        printf("Enter Name");
-        scanf("%s", &Name);
-        printf("Enter Age");
-        scanf("%s", &Age);
-        printf("Enter Blood Group");
-        scanf("%s", &B_Group);
-        printf("Enter Height(cm)");
+        printf("Enter S_no\n");
+        scanf("%s", S_No);
+        printf("Enter Name\n");
+        scanf("%s", Name);
+        printf("Enter Age\n");
+        scanf("%s", Age);
+        printf("Enter Blood Group\n");
+        scanf("%s", B_Group);
+        printf("Enter Height(cm)\n");
         scanf("%f", &height);
-        printf("Enter Weight(kg)");
+        printf("Enter Weight(kg)\n");
         scanf("%f", &weight);
-        bmi = weight * 10000 / height * height;
-        char pt_inf[7] = {S_No, Name, Age, B_Group, height, weight, bmi};
+        bmi = weight * 10000 / (height * height);
+        char pt_inf[7][20];
+        sprintf(pt_inf[0], "%s", S_No);
+        sprintf(pt_inf[1], "%s", Name);
+        sprintf(pt_inf[2], "%s", Age);
+        sprintf(pt_inf[3], "%s", B_Group);
+        sprintf(pt_inf[4], "%.2f", height);
+        sprintf(pt_inf[5], "%.2f", weight);
+        sprintf(pt_inf[6], "%.2f", bmi);
         add_patient(list, pt_inf, size);
-
+        show_list(ptr, size);
+        goto main_menu;
+    case 5:
+        break;
     default:
+        printf("Error Try again");
         goto main_menu;
     }
 }
@@ -52,7 +67,7 @@ main_menu:
 void show_list(char (*ptr)[7][20], int size)
 {
     int i, j;
-    for (i = 0; i < size; i++)
+    for (i = 0; i < size + 1; i++)
     {
         for (j = 0; j < 7; j++)
         {
@@ -66,10 +81,10 @@ void show_patient(char (*ptr)[7][20], int Sno)
     int i;
     for (i = 0; i < 7; i++)
     {
-        printf("%s", ptr[Sno][i]);
+        printf("%s\t", ptr[Sno][i]);
     }
 }
-void add_patient(char (*ptr)[7][20], char pt_info[7], int size)
+void add_patient(char (*ptr)[7][20], char pt_info[7][20], int size)
 {
     int i, j;
     for (j = 0; j < 7; j++)
